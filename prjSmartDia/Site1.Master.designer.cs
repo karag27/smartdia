70405d3788663729ba9cf0a08195b8ce0d7dc7a9
AKDMK477 <AKDMK477@AKDMK277>
2019-11-30 13:48:05 +0200

    Fix

--- prjSmartDia/Site1.Master.designer.cs
+++ prjSmartDia/Site1.Master.designer.cs
@@ -31,15 +31,6 @@ namespace prjSmartDia {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// TopHeader control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.TopHeader TopHeader;
-        
-        /// <summary>
         /// TopMenu control.
         /// </summary>
         /// <remarks>
@@ -49,85 +40,13 @@ namespace prjSmartDia {
         protected global::prjSmartDia.UserControls.TopMenu TopMenu;
         
         /// <summary>
-        /// Banner1 control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::prjSmartDia.UserControls.Banner1 Banner1;
-        
-        /// <summary>
-        /// Slogan control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Slogan Slogan;
-        
-        /// <summary>
-        /// Detail1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Detail1 Detail1;
-        
-        /// <summary>
-        /// Disease control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Disease Disease;
-        
-        /// <summary>
-        /// istatical control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.istatical istatical;
-        
-        /// <summary>
-        /// Team control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Team Team;
-        
-        /// <summary>
-        /// Banner2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Banner2 Banner2;
-        
-        /// <summary>
-        /// Map control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.Map Map;
-        
-        /// <summary>
-        /// FormContact control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::prjSmartDia.UserControls.FormContact FormContact;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
         
         /// <summary>
         /// Footer control.
--- prjSmartDia/wfHomePage.aspx
+++ prjSmartDia/wfHomePage.aspx
@@ -1,2 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="wfHomePage.aspx.cs" Inherits="prjSmartDia.WebForm1" %>
 
+<%@ Register Src="~/UserControls/Banner1.ascx" TagPrefix="uc1" TagName="Banner1" %>
+<%@ Register Src="~/UserControls/Slogan.ascx" TagPrefix="uc1" TagName="Slogan" %>
+<%@ Register Src="~/UserControls/Detail1.ascx" TagPrefix="uc1" TagName="Detail1" %>
+<%@ Register Src="~/UserControls/Disease.ascx" TagPrefix="uc1" TagName="Disease" %>
+<%@ Register Src="~/UserControls/Team.ascx" TagPrefix="uc1" TagName="Team" %>
+
+<asp:Content ID="MainContent1" ContentPlaceHolderID="MainContent" runat="server">
+    <uc1:Banner1 runat="server" ID="Banner1" />
+    <uc1:Slogan runat="server" ID="Slogan" />
+    <uc1:Detail1 runat="server" ID="Detail1" />
+    <uc1:Disease runat="server" ID="Disease" />
+    <uc1:Team runat="server" ID="Team" />
+
+</asp:Content>
