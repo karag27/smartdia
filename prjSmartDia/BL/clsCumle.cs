51fe50661238da8975aa37087e6cbcc0fa1b2073
Yusuf <Yusuf@DESKTOP-FFOHKPK>
2019-12-01 00:36:11 +0300

    fix

--- prjSmartDia/BL/clsCumle.cs
+++ prjSmartDia/BL/clsCumle.cs
@@ -6,22 +6,7 @@ using System.Web;
 
     public class clsCumle
     {
-    private string[] _sKelimeler;
 
-    public string[] sKelimeler
-    {
-        get { return _sKelimeler; }
-        set { _sKelimeler = value; }
-    }
-
-    //method kelimeleri ayırma kelimeleri parçalama 
-    //bağlaçlarda ayırma yontemi
-    // string[] sAyracListe = { "." };
-    // char[] sKirliAyrac = { '_', '-', '!', '<', '>', '\'', '\u0022', '+', '/', '*', '|','?' };
-    //   string[] sEdat= {"ve", "ile", "de", "ki","gibi","ama","ancak","fakat","bazı","bir","bu","çok","diğer", "diye","gibi","dolayı","eğer","falan","filan","hangi","hem","beri","bile","böyle","böylece","göre","halen","hatta","hep","her","hiç","için","ise","kadar","madem","mı","mu","mü","o","öbür","öyle","oysa","pek","rağmen","şayet","şey","şu","tamam","tüm","var","yine","yoksa","zaten","zira" };
-    //Cumleyi kelimelere bölecek
-    //String dizi . ayraçlar
-    //Cumleleri . ayırma 
     string[] _sAyracListe;
 
     public string[] sAyracListe
@@ -36,104 +21,82 @@ using System.Web;
         get { return _sKirliAyrac; }
         set { _sKirliAyrac = value; }
     }
-    string[] _sEdat;
-    public string[] sEdat
-    {
-        get { return _sEdat; }
-        set { _sEdat = value; }
-    }
+  
 
-    private List<string> _sCumleler;
+    private string[] _sCumleler;
 
-    public List<string> sCumleler
+    public string[] sCumleler
     {
         get { return _sCumleler; }
         set { _sCumleler = value; }
     }
 
 
-
-
-
-
-
-
-
-
     public void EdatTemizle()
     {
-
-        for (int k = 0; k < sKelimeler.Length; k++)
+        for(int j=0;j<sCumleler.Length;j++)
         {
-         //   char[] cHarf = sKelimeler[k].Trim().ToCharArray();
-            string sYeniKelime = "";
-
-            for (int i = 0; i < sEdat.Length; i++)
+            for (int i = 0; i < sKirliAyrac.Length; i++)
             {
-                char[] sEdatParcala = sEdat[i].ToCharArray();
-                int iSayac = 0;
-                for (int j = 0; j <sKelimeler[k].Trim().Length; j++)
+                if (( sCumleler[j].IndexOf(sKirliAyrac[i]))!=-1)
                 {
-
-                    if (sKelimeler[k].ElementAt(j) == sEdatParcala[i])
+                  char[] cDonustur = sCumleler[j].ToCharArray();
+                    int deger=sCumleler[j].IndexOf(sKirliAyrac[i]);
+                    if (cDonustur[deger - 1]!=' ' && cDonustur[deger+ sKirliAyrac.Length+1] != ' ')
                     {
-                        iSayac++;
-                    }
+                        sCumleler[j] = sCumleler[j].Replace(sKirliAyrac[i], ' ');
+                    } 
+
+                    
 
                 }
 
-                if (iSayac== sEdatParcala.Length)
-                {
-                    sKelimeler[k] ="";
-                    sYeniKelime = "";
-                    break;
-
-                }
-                else
-                {
-                    sKelimeler[k] = sYeniKelime;
-                    sYeniKelime = "";
-                }
             }
 
-
-
         }
-
-
-
-
-
-
+       
     }
 
 
-    public void KelimeAyir()
+ 
+
+    public void bosluklariAl()
     {
-        foreach (string item in sCumleler)
-        {
+      
+            for (int i = 0; i < sCumleler.Length; i++)
+            {
+               
 
-            sKelimeler = item.Split(' ');
+            while (sCumleler[i].IndexOf("  ") != -1)
+            {
 
-
+                sCumleler[i] = sCumleler[i].Replace("  ", " ");
+            }
         }
+       
     }
 
+    
 
-    public void CumleleriAyir(string sParagraf)
+
+    public string[] CumleleriAyir(string sParagraf)
     {
 
         foreach (char  item in sKirliAyrac)
         {
-            sParagraf.Replace(item,' ').Trim();
+            sParagraf=sParagraf.Replace(item,' ');
+
         }
 
        
         foreach (string item in sAyracListe)
         {
-          
-            sCumleler = sParagraf.Split(Convert.ToChar(item)).ToList();
+            sCumleler = sParagraf.Split(Convert.ToChar(item));
         }
+
+        EdatTemizle();
+        bosluklariAl();
+        return sCumleler;
 
     }
 
