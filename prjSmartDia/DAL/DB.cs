dd6ae087ded10f9136e5ecb5b4a04e6865c15e75
NemesiS <NemesiS@DESKTOP-D4QADK6>
2019-11-30 17:27:41 +0300

    fix

---
+++ prjSmartDia/DAL/DB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+public class DB
+{
+    public  DB()
+    {
+
+    }
+
+    public void RunQuery(string sQuery)
+    {
+
+    }
+
+    public DataTable RunQueryReturnDataTable(string sQuery)
+    {
+        DataTable dtData = null;
+
+
+        return dtData;
+    }
+}
