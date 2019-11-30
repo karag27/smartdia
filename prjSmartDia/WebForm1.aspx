7d03655e4e6bd6eb76f391ab894e8fa375f45b1c
AKDMK477 <AKDMK477@AKDMK277>
2019-11-30 12:49:23 +0200

    Fix

---
+++ prjSmartDia/WebForm1.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="prjSmartDia.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
--- prjSmartDia/WebForm1.aspx.designer.cs
+++ prjSmartDia/WebForm1.aspx.designer.cs
@@ -11,16 +11,7 @@ namespace prjSmartDia
 {
 
 
-    public partial class wfContactUs
+    public partial class WebForm1
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
